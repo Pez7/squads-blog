987f32131125588d35ea1d8dc5df1dae47c3c90b
Maria Paz <maria.pazrojasc@gmail.com>
2017-05-07 14:53:53 -0300

    arreglo con las integrantes del squad

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+	var miembro = [
+	{
+		"name":"Gilia Gongora",
+		"age":28,
+		"hobby1":"Leer",
+		"hobby2":"Escuchar música",
+		"hobby3":"Ver Pokémon",
+	},
+	{
+		"name":"Valentina Lopez",
+		"age":25,
+		"hobby1":"Tarot",
+		"hobby2":"Idiomas",
+		"hobby3":"Películas mudas",
+	},
+	{
+		"name":"Valentina Toledo",
+		"age": 20,
+		"hobby1":"Jugar fútbol",
+		"hobby2":"Escuchar música",
+		"hobby3":"Actividades al aire libre",
+	},
+	{
+		"name":"Trinidad Fuentes",
+		"age":26
+		"hobby1":"Video juegos",
+		"hobby2":"PugLover",
+		"hobby3":"Dormir",
+	},
+	{
+		"name":"Diana Monsalve",
+		"age":29,
+		"hobby1":"Mirar memes",
+		"hobby2":"Hacer negocios",
+		"hobby3":"Comer tablitas",
+	},{
+		"name":"Daniela Sanchez",
+		"age":24,
+		"hobby1":"Escuchar música",
+		"hobby2":"Cantar",
+		"hobby3":"Ver series",
+	},{
+		"name":"María Paz Rojas",
+		"age":26,
+		"hobby1":"Escuchar música",
+		"hobby2":"Comer",
+		"hobby3":"Tomar cervecita",
+	},{
+		"name":"Karina Palacios",
+		"age":24,
+		"hobby1":"Ver series",
+		"hobby2":"Leer",
+		"hobby3":"Escuchar música",
+	},{
+		"name":"Maribel Díaz",
+		"age":30,
+		"hobby1":"Salir a tomar café",
+		"hobby2":"Comer muffins",
+		"hobby3":"El silencio y escribir",
+	}
+];
